057cf5d164cf9caa22fa2811a001d7a2609ef7dd
shed4329 <shed4329@outlook.com>
2025-12-10 17:39:47 +0800

    test with some compression software and found some zip compressed doesn't support

--- test/test5.cpp
+++ test/test5.cpp
@@ -19,46 +19,46 @@ void func(){
     shed_std::Ffile_input  fin;
 
     // 讀取二進制文件
-    shed_std::Vvector<shed_zip::uint8_t> image_hex_read;
-    if(fin.open("chart.svg")){
-        // 64KB
-        for(int i = 0; i< 64*1024/1024; ++i){
-            char buffer[1024];
-            //shed_std should provide some file API?
-            int len = fin.read_n(buffer,1024,false);
-            for(int j = 0;j<len;++j) image_hex_read.push_back(buffer[j]);
-        } 
-    }else{
-        shed_std::Cconsole_output << "找不到文件" <<shed_std::end_line;
-    }
-    shed_std::Cconsole_output << "读取的文件大小="<<image_hex_read.size()<<shed_std::end_line;
-    // 2.壓縮
-    shed_zip::ZipConfig cfg(6);
-    shed_zip::ZipArchiver archiver(cfg);
-    auto zip_data = archiver.create_gzip(image_hex_read,"chart.svg");
+    // shed_std::Vvector<shed_zip::uint8_t> image_hex_read;
+    // if(fin.open("chart.svg")){
+    //     // 64KB
+    //     for(int i = 0; i< 64*1024/1024; ++i){
+    //         char buffer[1024];
+    //         //shed_std should provide some file API?
+    //         int len = fin.read_n(buffer,1024,false);
+    //         for(int j = 0;j<len;++j) image_hex_read.push_back(buffer[j]);
+    //     } 
+    // }else{
+    //     shed_std::Cconsole_output << "找不到文件" <<shed_std::end_line;
+    // }
+    // shed_std::Cconsole_output << "读取的文件大小="<<image_hex_read.size()<<shed_std::end_line;
+    // // 2.壓縮
+    // shed_zip::ZipConfig cfg(6);
+    // shed_zip::ZipArchiver archiver(cfg);
+    // auto zip_data = archiver.create_gzip(image_hex_read,"chart.svg");
 
-    // 3.寫入到壓縮文件
-    if(fout.open("test5.zip",false)){
-        shed_std::Sstring str;
-        for(int i = 0;i<zip_data.size();++i) str.push_back(zip_data[i]);
-        fout.put_chars(str.c_string(),str.size(),false);
-        if(fout.is_good()){
-            shed_std::Cconsole_output<<"压缩文件创建成功!"<<shed_std::end_line;
-        }else{
-            shed_std::Cconsole_output<<"压缩文件创建失败!"<<shed_std::end_line;
-        } 
-    }else{
-        shed_std::Cconsole_output << "无法打开文件!"<<shed_std::end_line;
-    }
-    // 关闭流
-    fout.close();
-    fin.close();
+    // // 3.寫入到壓縮文件
+    // if(fout.open("test5.zip",false)){
+    //     shed_std::Sstring str;
+    //     for(int i = 0;i<zip_data.size();++i) str.push_back(zip_data[i]);
+    //     fout.put_chars(str.c_string(),str.size(),false);
+    //     if(fout.is_good()){
+    //         shed_std::Cconsole_output<<"压缩文件创建成功!"<<shed_std::end_line;
+    //     }else{
+    //         shed_std::Cconsole_output<<"压缩文件创建失败!"<<shed_std::end_line;
+    //     } 
+    // }else{
+    //     shed_std::Cconsole_output << "无法打开文件!"<<shed_std::end_line;
+    // }
+    // // 关闭流
+    // fout.close();
+    // fin.close();
 
     // 4.读取zip
     shed_std::Vvector<shed_zip::uint8_t> zip_hex_read;
     if(fin.open("test5.zip")){
         // 64KB
-        for(int i = 0; i< 64*1024/1024; ++i){
+        for(int i = 0; i< 8192*1024/1024; ++i){
             char buffer[1024];
             //shed_std should provide some file API?
             int len = fin.read_n(buffer,1024,false);
@@ -71,6 +71,7 @@ void func(){
     // 5.解压缩
     shed_zip::UnzipExtractor extractor;
     auto extracted_res = extractor.extract(zip_hex_read);
+    shed_std::Cconsole_output << extracted_res.size() <<shed_std::end_line;
 
     // 6.写出到文件
     if(fout.open("chart2.svg",false)){
